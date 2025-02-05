c39acb3872b754e362cf047bcaa072d8452b58c3
ashiquegeorge15 <ashiquegeorge15@gmail.com>
2025-02-05 23:16:41 +0530

    almost completed the functionality updtated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography, Link } from '@mui/material';
 import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => (
-  <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+  <Box mt="80px" bgcolor="#FFF3F4" py="24px">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px">
+      <Typography variant="body2" color="textSecondary" align="center">
+        © {new Date().getFullYear()} My Company. All rights reserved.
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Link href="#" color="inherit" underline="hover">
+          Privacy Policy
+        </Link>
+        <Link href="#" color="inherit" underline="hover">
+          Terms of Service
+        </Link>
+        <Link href="#" color="inherit" underline="hover">
+          Contact Us
+        </Link>
+      </Stack>
     </Stack>
   </Box>
 );
